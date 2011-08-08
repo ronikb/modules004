31b6ded1815f87e60ddbe0580c69c3a41fc9e2f9
root <root@ip-10-99-31-29.ec2.internal>
2011-08-08 08:19:18 +0000

    upload from server

--- application/manifests/init.pp
+++ application/manifests/init.pp
@@ -1,4 +1,4 @@
 # This is a core file of application module
 class application {
-	include application::config
+	include application::config, application::gitcloneapp, application::dbcreate, application::dbrestore, application::phpmemory, application::symlink, application::editfor_cleanurl, application::editfor_documentroot
 }
